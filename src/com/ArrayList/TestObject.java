0163c5ac002d7fd46426111a68c14d63de4ab49b
Dharmesh <dharmesh1989s@gmail.com>
2019-11-21 11:04:24 +0530

    p

---
+++ src/com/ArrayList/TestObject.java
@@ -0,0 +1,13 @@
+package com.ArrayList;
+
+public class TestObject {
+	public String a;
+	public String  b;
+	public TestObject(String a, String b) {
+		super();
+		this.a = a;
+		this.b = b;
+	}
+	
+
+}
